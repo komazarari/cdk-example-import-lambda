c32796133b48bc19e092fb72f3041760afe413b0
Takuto Komazaki <komazarari@gmail.com>
2020-04-27 15:16:08 +0000

    imported

--- lib/cdk-example-import-lambda-stack.ts
+++ lib/cdk-example-import-lambda-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
+import * as myfunc from '@komazarari/cdk-example-lambda-with-layer';
 
 export class CdkExampleImportLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new myfunc.CdkExampleLambdaWithLayer(this, 'myFunc', { myMessage: 'Imported!'});
   }
 }
